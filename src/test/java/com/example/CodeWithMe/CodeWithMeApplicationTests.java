4edcffb81115be7d448fb9bc4173a65321fd208f
aditya-bhosale <bhosale.aditya1996@gmail.com>
2025-04-01 21:26:06 +0530

    Initial Commit

---
+++ src/test/java/com/example/CodeWithMe/CodeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
